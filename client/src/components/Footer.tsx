23373dd82dcd2470da3dd1ca340217b356147f23
Vignesh Murugan <vigmurug@gmail.com>
2023-08-24 17:52:25 +0530

    Create Footer.tsx

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div>
+            <p className="text-sm">Made with ❤️ by <Link href={'https://www.webdev-vignesh.tech'}>Vignesh</Link></p>
+        </div>
+    )
+}
+
+export default Footer;
