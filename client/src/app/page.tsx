6a03fbee31a20c1e99ad78b767cc8d772e4416a7
Vignesh Murugan <vigmurug@gmail.com>
2023-08-24 19:17:34 +0530

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="p-4" style={{backgroundImage: `url(${BackImg})`}}>
+    <main className="p-4 bg-gradient-to-r from-blue-800 to-indigo-900" style={{backgroundImage: `url(${BackImg})`}}>
       <nav className='flex justify-between'>
         <div>
           <p>Streak⚡Stack</p>
@@ -21,12 +21,13 @@ export default function Home() {
       </nav>
       <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col'>
         <div>
-          <p className='text-5xl text-center mb-8'>Streak⚡Stack</p>
-          <p className='text-lg text-center'>Build Habits, Stack Success</p>
-          <p className='text-lg text-center'> Elevate Your Life with Streak Stack!</p>
+          <p className='text-5xl text-center font-bold'>Build Habits</p>
+          <p className='text-5xl text-center mb-8 font-bold text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-red-600'>Stack Success!</p>
+          <p className='text-lg text-center'>Discover your path to success with Streak Stack - the ultimate habit tracking app.</p>
+          <p className='text-lg text-center'>Transform your life one streak at a time!</p>
         </div>
-        <div className='text-center'>
-          <button className='mt-4 border border-blue-500 bg-blue-500 p-2 rounded-md text-white'>Get started for free</button>
+        <div className='mt-20 text-center'>
+          <button className='border border-green-500 bg-green-500 p-2 rounded-md text-white'>Get started for free</button>
         </div>
       </div>
       <div className='absolute bottom-0 left-1/2 trasnform -translate-x-1/2 -translate-y-1/2'>
