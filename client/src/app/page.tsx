8134bbd2cc87e8c1a6b1d0c948485a98933c376c
Vignesh Murugan <vigmurug@gmail.com>
2023-08-24 17:52:44 +0530

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,12 +3,13 @@ import { Inter } from 'next/font/google'
 import { AiFillGithub } from "react-icons/ai";
 import Link from 'next/link';
 import Footer from "../components/Footer";
+import BackImg from "../../public/bg.jpg";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="p-4">
+    <main className="p-4" style={{backgroundImage: `url(${BackImg})`}}>
       <nav className='flex justify-between'>
         <div>
           <p>Streak⚡Stack</p>
@@ -20,9 +21,12 @@ export default function Home() {
       </nav>
       <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col'>
         <div>
-          <p className='text-3xl text-center'>Streak⚡Stack</p>
+          <p className='text-5xl text-center mb-8'>Streak⚡Stack</p>
           <p className='text-lg text-center'>Build Habits, Stack Success</p>
           <p className='text-lg text-center'> Elevate Your Life with Streak Stack!</p>
+        </div>
+        <div className='text-center'>
+          <button className='mt-4 border border-blue-500 bg-blue-500 p-2 rounded-md text-white'>Get started for free</button>
         </div>
       </div>
       <div className='absolute bottom-0 left-1/2 trasnform -translate-x-1/2 -translate-y-1/2'>
