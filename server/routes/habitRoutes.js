7d45699e1220f47a0b52ff8aa770494787f9baf0
Vignesh Murugan <vigmurug@gmail.com>
2023-09-06 14:48:23 +0530

    Update habitRoutes.js

--- server/routes/habitRoutes.js
+++ server/routes/habitRoutes.js
@@ -2,10 +2,5 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    const data = db.collection("habitsData").find().toArray();
+    const data = db.collection("sample").find().toArray();
     res.json(data);
-<<<<<<< HEAD
-})
-=======
-})
->>>>>>> 1e961376259e266331cc44fed9da050acbf05fdd
