375ca3da167956963f1e0765f4c9711b4b2033fe
Vignesh Murugan <vigmurug@gmail.com>
2023-09-06 14:49:18 +0530

    Create habitModel.js

---
+++ server/models/habitModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const habitSchema = mongoose.Schema(
+    {
+        habitName: {
+            type: String,
+            required: [true, "Please enter the habit name"],
+        },
+        habitDescription: {
+            type: String,
+            required: true,
+        },
+        count: {
+            type: Number,
+            required: false,
+            default: 0,
+        },
+        goal: {
+            type: Number,
+            required: true,
+        },
+        frequency: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Habit = mongoose.model("habitData", habitSchema);
+
+module.exports = Habit;
