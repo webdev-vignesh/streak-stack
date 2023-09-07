699eb5d06129890d977f5c45245b07911dfaa3a9
Vignesh Murugan <vigmurug@gmail.com>
2023-09-07 21:56:53 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,92 +1,25 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
-const connectDb = require('./config/dbConnection');
-const Habit = require("./models/habitModel");
 const cors = require("cors");
-// const habitRouter = require("./routes/habitRoutes");
+const dotenv = require('dotenv').config();
+
+const connectDb = require('./config/dbConnection');
+const habitRoute = require("./routes/habitRoute");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
-
-// get all habits
-app.get("/api/habitRecords", async(req, res) => {
-    try {
-        const habits = await Habit.find({});
-        res.status(200).json(habits);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-// get a habit with id
-app.get("/api/habitRecords/:id", async(req, res) => {
-    try {
-        const {id} = req.params;
-        const habits = await Habit.findById(id);
-        res.status(200).json(habits);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-// add a new habit record
-app.post('/api/habitRecords', async(req, res) => {
-    try {
-        const habits = await Habit.create(req.body)
-        res.status(200).json(habits);
-        
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
-})
-
-// update a new habit record
-app.put('/api/habitRecords/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const habits = await Habit.findByIdAndUpdate(id, req.body)
-        // if data with given id not found
-        if(!habits){
-            return res.send(404).json({message: `cannot find any record with ${id}`});
-        }
-        const updatedHabit = await Habit.findById(id);
-        res.status(200).json(updatedHabit);
-        
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
-})
-
-// delete a new habit record
-app.delete('/api/habitRecords/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const habits = await Habit.findByIdAndDelete(id);
-        // if data with given id not found
-        if(!habits){
-            return res.send(404).json({message: `cannot find any record with ${id}`});
-        }
-        res.status(200).json(habits);
-        
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
-})
-
+// middleware for routes
+app.use('/api/habitRecords', habitRoute);
 
 // connecting to mongodb from local server
 connectDb()
 .then(() => {
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`)
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
     })
 })
 .catch((error) => {
