0cc4fb7d891facfd9260dcea67d51cfec44723c3
Vignesh Murugan <vigmurug@gmail.com>
2023-09-15 08:15:56 +0530

    ui changes

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,22 +1,26 @@
+'use client';
+
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { AiFillGithub } from "react-icons/ai";
 import { FaRegCircleUser } from "react-icons/fa6";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const Header = () => {
 
     const router = useRouter();
+    const pathName = usePathname();
+
     return(
-        <div className="header flex justify-between p-4">
+        <div className="header flex justify-between p-4 bg-gradient-to-r from-blue-800 to-indigo-900 dark:bg-gradient-to-r dark:from-blue-800 dark:to-indigo-900">
             <div className="logo">
                 {/* <Image src={} alt="streak-stack logo" /> */}
                 <p onClick={() => {router.push("/")}} className="hover: cursor-pointer">Streak⚡Stack</p>
             </div>
             <div className="flex items-center space-x-4">
-                <Link href={"/"}>Go Pro</Link>
-                <p className="hover: cursor-pointer"><FaRegCircleUser /></p>
+                <Link href={"/"}>{ pathName === "/" ? "Pricing" : "Go Pro"}</Link>
+                <p className="hover: cursor-pointer">{pathName === "/" ? <AiFillGithub /> : <FaRegCircleUser />}</p>
+                <p></p>
             </div>
         </div>
     )
