350b33d7ac75ae65fa9421f9e1a90e718b0e46c2
Vignesh Murugan <vigmurug@gmail.com>
2023-09-15 08:13:29 +0530

    removed header

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Header from "../../components/header";
 import Footer from "../../components/footer";
 import AddRecords from "@/components/addRecords";
 import HabitCard from "@/components/habitCard";
@@ -43,38 +42,39 @@ const Dashboard = () => {
   }, [handleFetch])
 
   return (
-    <div className="bg-gradient-to-r from-blue-800 to-indigo-900 dark:bg-gradient-to-r dark:from-blue-800 dark:to-indigo-900">
-        <Header />
-        <div className="body p-2">
-          <div className="">
-            <div className="flex justify-between items-center mb-2">
-              <div className="flex space-x-2">
-                <div className="ml-2 p-2 border rounded-md bg-white text-black h-1/2 hover:cursor-pointer">
-                  Dashboard
+    <div>
+      <div className="bg-gradient-to-r from-blue-800 to-indigo-900 dark:bg-gradient-to-r dark:from-blue-800 dark:to-indigo-900">
+          <div className="body p-2">
+            <div className="">
+              <div className="flex justify-between items-center mb-2">
+                <div className="flex space-x-2">
+                  <div className="ml-2 p-2 border rounded-md bg-white text-black h-1/2 hover:cursor-pointer">
+                    Dashboard
+                  </div>
+                  <div className="ml-2 p-2 border rounded-md bg-gray-500 text-black h-1/2 hover:cursor-pointer">
+                    All Habits
+                  </div>
+                  <div className="ml-2 p-2 border rounded-md bg-gray-500 text-black h-1/2 hover:cursor-pointer">
+                    Achievements
+                  </div>
                 </div>
-                <div className="ml-2 p-2 border rounded-md bg-gray-500 text-black h-1/2 hover:cursor-pointer">
-                  All Habits
-                </div>
-                <div className="ml-2 p-2 border rounded-md bg-gray-500 text-black h-1/2 hover:cursor-pointer">
-                  Achievements
+                <div>
+                  <AddRecords handleFetch={handleFetch} setHandleFetch={setHandleFetch} />
                 </div>
               </div>
-              <div>
-                <AddRecords handleFetch={handleFetch} setHandleFetch={setHandleFetch} />
-              </div>
-            </div>
-            <div className="text-white">
-              <div className="p-2 grid grid-cols-1 sm:grid-cols-2 gap-4">
-                {records?.map((record) => (
-                  <HabitCard key={record._id} record={record} handleFetch={handleFetch} setHandleFetch={setHandleFetch} />
-                ))}
+              <div className="text-white">
+                <div className="p-2 grid grid-cols-1 sm:grid-cols-2 gap-4">
+                  {records?.map((record) => (
+                    <HabitCard key={record._id} record={record} handleFetch={handleFetch} setHandleFetch={setHandleFetch} />
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className={`text-center ${pos} bottom-0 left-1/2 trasnform -translate-x-1/2 -translate-y-1/2`}>
+          <div className={`text-center ${pos} bottom-0 left-1/2 trasnform -translate-x-1/2 -translate-y-1/2`}>
             <Footer />
-        </div>
+          </div>
+      </div>
     </div>
   )
 }
