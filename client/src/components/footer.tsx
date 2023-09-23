0329490417d658c988a15cf5e5cec2ca2b52a0f9
Vignesh Murugan <vigmurug@gmail.com>
2023-09-23 19:05:41 +0530

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
     return(
-        <div>
-            <p className="text-sm">Made with ❤️ by <a href={"https://www.webdev-vignesh.tech"} target="_blank">Vignesh</a></p>
+        <div className="text-center absolute bottom- inset-x-0 md:relative md:bottom-0 md:mt-4 md:mb-4">
+            <p className="text-sm">Made with ❤️ by <Link className="hover:underline text-red-500" href={"https://www.webdev-vignesh.tech"} target="_blank">Vignesh</Link></p>
         </div>
     )
 }
