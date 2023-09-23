8acb790e4de64879c564c23dc7448d4d1289f295
Vignesh Murugan <vigmurug@gmail.com>
2023-09-23 19:06:12 +0530

    Update header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -4,40 +4,38 @@ import React from "react";
 import Link from "next/link";
 import { AiFillGithub } from "react-icons/ai";
 import { FaRegCircleUser } from "react-icons/fa6";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import UserDropdown from "./userDropdown";
+import { useRouter } from 'next/router';
 
 const Header = () => {
 
-    const router = useRouter();
     const pathName = usePathname();
 
     const { status, data: session} = useSession();
 
     return(
-        <div className="header flex justify-between p-4 bg-gradient-to-r from-blue-800 to-indigo-900 dark:bg-gradient-to-r dark:from-blue-800 dark:to-indigo-900">
+        <div className="header flex justify-between items-center  p-3 bg-white dark:bg-gray-100 text-blue-600 ">
             <div className="logo">
                 {/* <Image src={} alt="streak-stack logo" /> */}
-                <p onClick={() => {router.push("/")}} className="hover: cursor-pointer">Streak⚡Stack</p>
+                <Link href={'/'} className="hover: cursor-pointer font-bold text-lg md:text-2xl">⚡Streak Stack</Link>
             </div>
-            <div className="flex items-center space-x-4">
-                <Link href={"/"}>
-                    { 
-                        (pathName === "/" || pathName === "/signIn" )  
-                            ? "Pricing" 
-                            : "Go Pro"
-                    }
+            <div className="flex items-center space-x-2 md:space-x-6 text-sm md:text-lg font-medium">
+                <Link href={"/pricing"} className="hover:underline">
+                    Pricing
+                </Link>
+                <Link href={"/signIn"} className="hover:underline">
+                    Sign In
                 </Link>
                 <div className="hover: cursor-pointer">
                     {
-                        (pathName === "/"|| pathName === "/signIn" ) 
-                        ? 
-                            <Link href={'https://github.com/webdev-vignesh/streak-stack'} target="_blank">
-                                <AiFillGithub />
-                            </Link> 
-                        : <><UserDropdown /></>
+                        (pathName === "/"|| pathName === "/signIn" || pathName === "/signUp"  || pathName === "/pricing" ) 
+                            &&
+                        <Link href={'/signUp'} className="bg-blue-500 text-white p-2 rounded-3xl " >
+                            Try for Free
+                        </Link> 
                     }
                 </div>
                 <p></p>
