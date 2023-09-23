0d12e3bbfdc62ced7b5e8d244b6edba7dddfcaec
vigmururg <vigmurug@gmail.com>
2023-09-23 18:50:32 +0530

    commit

--- client/src/components/signInBtn.tsx
+++ client/src/components/signInBtn.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import GoogleLogo from "@/./../public/google-logo.png";
+import GoogleLogo from "@/../public/google-logo.png";
 import Image from "next/image";
 import { signIn, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const SignInBtn = () => {
 
     const { status } = useSession();
     const router: any = useRouter();
+    const pathName = usePathname();
 
     return(
         <div className="flex justify-center items-center ">
@@ -17,12 +18,12 @@ const SignInBtn = () => {
                     router.push('/dashboard')
                 :
                     <button 
-                className="rounded-lg flex items-center shadow-xl bg-white dark:bg-white"
+                className="rounded-lg flex items-center shadow-md bg-white dark:bg-white"
                 onClick={() => {signIn("google")}}
                 >
-                        <Image src={GoogleLogo} alt="Google" className=" m-2" height={30} width={30} />
-                        <span className="bg-green-500 text-white px-4 py-3 rounded-e-lg">
-                            Sign In with Google
+                        <Image src={GoogleLogo} alt="Google" className=" mx-2" height={30} width={30} />
+                        <span className="bg-blue-500 text-white px-4 py-2 rounded-e-lg">
+                            Sign <span>{pathName === "/signUp" ? "Up" : "In"}</span> with Google
                         </span>
                     </button>
                 }
