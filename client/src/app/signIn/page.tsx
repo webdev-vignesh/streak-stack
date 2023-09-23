1d80837517e29ba280a66261219eb581dc7a011e
Vignesh Murugan <vigmurug@gmail.com>
2023-09-23 19:03:47 +0530

    created signin page card

--- client/src/app/signIn/page.tsx
+++ client/src/app/signIn/page.tsx
@@ -4,6 +4,8 @@ import GoogleLogo from "@/./../public/google-logo.png";
 import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import SignInBtn from "@/components/signInBtn";
+import Link from "next/link";
 
 const SignIn = () => {
 
@@ -11,22 +13,26 @@ const SignIn = () => {
     const router: any = useRouter();
 
     return(
-        <div className="flex justify-center items-center h-screen -mt-16 bg-gradient-to-r from-blue-800 to-indigo-900">
-            { status === "authenticated"
-                ?
-                    router.push('/dashboard')
-                :
-                    <button 
-                className="rounded-lg flex items-center shadow-xl bg-white"
-                onClick={() => {signIn("google")}}
-                >
-                        <Image src={GoogleLogo} alt="Google" className=" m-2" height={30} width={30} />
-                        <span className="bg-green-500 text-white px-4 py-3 rounded-e-lg">
-                            Sign In with Google
-                        </span>
-                    </button>
-                }
+        <div className='bg-blue-500 flex justify-center items-center h-screen'>
+        <div className='absolute top-1/4 md:relative md:top-0 flex flex-col justify-center items-center p-6 bg-white rounded-md shadow-md'>
+            <p className='text-2xl font-semibold'>Welcome back!</p>
+            <div className='flex flex-col space-y-4 mt-4 '>
+                <input type='text' className='p-3 bg-gray-100 placeholder:text-gray-400 rounded-md focus:outline-blue-500 focus:bg-white' placeholder='Email' />
+                <input type='text' className='p-3 bg-gray-100 placeholder:text-gray-400 rounded-md focus:outline-blue-500 focus:bg-white' placeholder='Password' />
+                <button type='submit' className='bg-blue-500 rounded-md text-white p-2'>Sign In</button>
+            </div>
+            <div className='flex items-center justify-center my-2'>
+                <p className='text-gray-500'>OR</p>
+            </div>
+            <div>
+                <SignInBtn />
+            </div>
+            <div className='flex mt-3 text-sm font-medium'>
+                <p>{"Don't have an account? \u00A0"}</p>
+                <Link href={'/signUp'} className='underline text-orange-500'>Sign Up</Link>
+            </div>
         </div>
+    </div>
     )
 }
 
