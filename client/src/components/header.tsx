94ad01cb6bab563cd34c9cf6ae3b9817e4da15f4
Vignesh Murugan <vigmurug@gmail.com>
2023-09-24 21:21:06 +0530

    Update header.tsx

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -9,6 +9,7 @@ import { useSession } from "next-auth/react";
 import Image from "next/image";
 import UserDropdown from "./userDropdown";
 import { useRouter } from 'next/router';
+import Dashboard from "@/app/dashboard/page";
 
 const Header = () => {
 
@@ -17,7 +18,7 @@ const Header = () => {
     const { status, data: session} = useSession();
 
     return(
-        <div className="header flex justify-between items-center  p-3 bg-white dark:bg-gray-100 text-blue-600 ">
+        <div className="header flex justify-between items-center  p-3 bg-white  text-blue-600 ">
             <div className="logo">
                 {/* <Image src={} alt="streak-stack logo" /> */}
                 <Link href={'/'} className="hover: cursor-pointer font-bold text-lg md:text-2xl">⚡Streak Stack</Link>
@@ -26,18 +27,41 @@ const Header = () => {
                 <Link href={"/pricing"} className="hover:underline">
                     Pricing
                 </Link>
-                <Link href={"/signIn"} className="hover:underline">
-                    Sign In
-                </Link>
-                <div className="hover: cursor-pointer">
-                    {
-                        (pathName === "/"|| pathName === "/signIn" || pathName === "/signUp"  || pathName === "/pricing" ) 
-                            &&
-                        <Link href={'/signUp'} className="bg-blue-500 text-white p-2 rounded-3xl " >
-                            Try for Free
-                        </Link> 
-                    }
-                </div>
+                {
+                    (pathName !== "/dashboard" && status !== 'authenticated')
+                    ?
+                    <>
+                        <Link href={"/signIn"} className="hover:underline">
+                            {
+                                (pathName === "/dashboard")
+                                ?   <FaRegCircleUser/>
+                                :   'Sign In'
+                            }
+                        </Link>
+                        <div className="hover: cursor-pointer">
+                            {
+                                (pathName === "/"|| pathName === "/signIn" || pathName === "/signUp"  || pathName === "/pricing" ) 
+                                    &&
+                                <Link href={'/signUp'} className="bg-blue-500 text-white p-2 rounded-3xl " >
+                                    Try for Free
+                                </Link> 
+                            }
+                        </div>
+                    </>
+                    :
+                    <>
+                        {
+                        (pathName !== '/dashboard')
+                        &&   
+                        <Link href={'/dashboard'} className=" hover:underline">
+                            Dashboard
+                        </Link>
+                        }
+                        <div className="hover:cursor-pointer">
+                            <FaRegCircleUser/>
+                        </div>
+                    </>
+                }
                 <p></p>
             </div>
         </div>
